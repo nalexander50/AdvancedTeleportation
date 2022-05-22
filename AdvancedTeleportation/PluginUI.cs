89dca05e9a34a0cdb6ad77cbd5f7afc7b2d615cf
Nick Alexander <nick.aaron.alexander@gmail.com>
2022-05-21 22:43:36 -0400

    Initial Commit (2)

---
+++ AdvancedTeleportation/PluginUI.cs
@@ -0,0 +1,102 @@
+﻿using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace AdvancedTeleportation
+{
+    // It is good to have this be disposable in general, in case you ever need it
+    // to do any cleanup
+    class PluginUI : IDisposable
+    {
+        private readonly Configuration configuration;
+
+        private readonly ImGuiScene.TextureWrap goatImage;
+
+        // this extra bool exists for ImGui, since you can't ref a property
+        private bool visible = false;
+
+        public bool Visible
+        {
+            get => this.visible;
+            set => this.visible = value;
+        }
+
+        // this extra bool exists for ImGui, since you can't ref a property
+        private bool settingsVisible = false;
+
+        public bool SettingsVisible
+        {
+            get => this.settingsVisible;
+            set => this.settingsVisible = value;
+        }
+
+        // passing in the image here just for simplicity
+        public PluginUI(Configuration configuration, ImGuiScene.TextureWrap goatImage)
+        {
+            this.configuration = configuration;
+            this.goatImage = goatImage;
+        }
+
+        public void Dispose()
+        {
+            this.goatImage.Dispose();
+        }
+
+        public void Draw()
+        {
+            // This is our only draw handler attached to UIBuilder, so it needs to be
+            // able to draw any windows we might have open.
+            // Each method checks its own visibility/state to ensure it only draws when
+            // it actually makes sense.
+            // There are other ways to do this, but it is generally best to keep the number of
+            // draw delegates as low as possible.
+
+            DrawMainWindow();
+            DrawSettingsWindow();
+        }
+
+        public void DrawMainWindow()
+        {
+            if (!Visible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(375, 330), ImGuiCond.FirstUseEver);
+            ImGui.SetNextWindowSizeConstraints(new Vector2(375, 330), new Vector2(float.MaxValue, float.MaxValue));
+            if (ImGui.Begin("My Amazing Window", ref this.visible, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+                ImGui.Text($"The random config bool is None");
+
+                if (ImGui.Button("Show Settings"))
+                {
+                    SettingsVisible = true;
+                }
+
+                ImGui.Spacing();
+
+                ImGui.Text("Have a goat:");
+                ImGui.Indent(55);
+                ImGui.Image(this.goatImage.ImGuiHandle, new Vector2(this.goatImage.Width, this.goatImage.Height));
+                ImGui.Unindent(55);
+            }
+            ImGui.End();
+        }
+
+        public void DrawSettingsWindow()
+        {
+            if (!SettingsVisible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(232, 75), ImGuiCond.Always);
+            if (ImGui.Begin("A Wonderful Configuration Window", ref this.settingsVisible,
+                ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+
+            }
+            ImGui.End();
+        }
+    }
+}
